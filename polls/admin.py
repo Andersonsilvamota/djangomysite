3c43d1e16ccb15def1618e8496a67d4b94878c52
Andersonsilvamota <andersonsilv.mota@gmial.com>
2020-09-28 20:18:21 -0300

    commit qualquer

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ('question_text', 'pub_date', 'maior_choice',)
+    readonly_fields = ('maior_choice',)
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,10 @@
 from datetime import timedelta
-
 from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField('Texto da questão', max_length=200, help_text='Informe o texto da questão.')
+    pub_date = models.DateTimeField('Data de publicação')
 
     def was_published_recently(self):
         ontem = timezone.now() - timedelta(days=1)
@@ -14,10 +13,20 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @property
+    def maior_choice(self):
+        maior = self.choice_set.aggregate(models.Max('votes'))['votes__max']
+        return self.choice_set.get(votes__exact=maior)
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField('Testo da escolha',max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Questão')
+    choice_text = models.CharField('Testo da escolha', max_length=200)
+    pub_date = models.DateTimeField('Data de publicação')
     votes = models.IntegerField('Nº de votos',default=0)
+
+    def was_published_recently_Choice(self):
+        ontem = timezone.now() - timedelta(days=1)
+        return self.pub_date >= ontem
 
     def __str__(self):
         return self.choice_text
