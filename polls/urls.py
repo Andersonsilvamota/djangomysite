360fa4883b754b610a3f903f5b9fc2a3a0dd1a14
Andersonsilvamota <andersonsilv.mota@gmial.com>
2020-10-01 21:29:43 -0300

    criando novas views e templates

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sobre', views.sobre, name='sobre'),
-    path('turmaa', views.turmaA, name='turmaA'),
-    path('turmab', views.turmaB, name='turmaB'),
+    path('<int:question_id>', views.detail, name='detail'),
+    #path('sobre', views.sobre, name='sobre'),
+    #path('turmaa', views.turmaA, name='turmaA'),
+    #path('turmab', views.turmaB, name='turmaB'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question
 
-def index(request):
-    return HttpResponse()
+def index(request, template_name="polls/index.html"):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, template_name, context)
 
+def detail(request, question_id, template_name="polls/detail.html"):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, template_name, context)
+'''
 def sobre(request):
     print('equipe pweb')
     return HttpResponse('minha equipe pweb:anso,mayk,fontineles')
@@ -13,3 +25,4 @@ def turmaA(request):
 def turmaB(request):
     print('')
     return  HttpResponse('Professor: Saulo')
+'''
